b2dab6872f84717706b252f2b8681dc283251743
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-05-19 15:58:11 +0200

    Añadir nuevo contacto

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -2,29 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import contactList from '../contacts';
 
 interface Contact {
-    name: String;
-    email: String;
-    phoneNumber: String;
-    image: String ;
+  name?: String;
+  email?: String;
+  phoneNumber?: String;
+  image?: String;
 }
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
 })
 export class ContactListComponent implements OnInit {
   contacts: Object[];
   newContact: Contact;
 
-  constructor() { }
+  constructor() {
+    this.newContact = {};
+  }
 
   ngOnInit() {
     this.contacts = contactList;
   }
 
-  addContact(){
+  addContact() {
     // add contact to contacts list
+    this.contacts.push(this.newContact);
     // clear inputs
+    this.newContact = {};
   }
 }
