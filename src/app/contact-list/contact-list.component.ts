cd655d6cc290cc9b0ecd810a1ddb4b81b590df8a
AlexP953 <alexperis95@gmail.com>
2021-05-20 14:38:57 +0200

    Commit 2

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import contactList from '../contacts';
 
 interface Contact {
-    name: String;
-    email: String;
-    phoneNumber: String;
-    image: String ;
+  name: String;
+  email: String;
+  phoneNumber: String;
+  image: String;
 }
 
 @Component({
@@ -13,9 +13,15 @@ interface Contact {
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
+
 export class ContactListComponent implements OnInit {
   contacts: Object[];
-  newContact: Contact;
+  newContact: Contact = {
+    name: '',
+    email: '',
+    phoneNumber: '',
+    image: ''
+  };
 
   constructor() { }
 
@@ -23,8 +29,15 @@ export class ContactListComponent implements OnInit {
     this.contacts = contactList;
   }
 
-  addContact(){
+  addContact() {
     // add contact to contacts list
+    this.contacts.push({ name: this.newContact.name, email: this.newContact.email, phoneNumber: this.newContact.phoneNumber, image: this.newContact.image });
+
     // clear inputs
+    this.newContact.name = '';
+    this.newContact.email = '';
+    this.newContact.phoneNumber = '';
+    this.newContact.image = '';
+
   }
 }
