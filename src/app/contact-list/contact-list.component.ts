3b65cf8de7fdde853cadc987e8264bbb32d6081f
Yuumi <69478059+yumyum0035@users.noreply.github.com>
2021-05-20 20:39:54 +0200

    Commit 4
    CSS wip

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -11,10 +11,11 @@ interface Contact {
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.scss']
 })
 export class ContactListComponent implements OnInit {
   contacts: Object[];
+  favorites: Object[];
   newContact: Contact = {
     name: "",
     email: "",
@@ -26,16 +27,28 @@ export class ContactListComponent implements OnInit {
 
   ngOnInit() {
     this.contacts = contactList;
-    console.log(this.contacts);
+    this.favorites = contactList;
+
+    console.log(`favorites arr: ${this.favorites.length}`);
   }
 
   addContact(contact:Contact){
     // add contact to contacts list
+    contact.image == ""? contact.image = "http://svgur.com/i/7aS.svg" : contact.image = contact.image;
     const newContact = { ...contact };
     this.contacts.push(newContact);
 
     // clear inputs
     this.handleClear();
+  }
+
+  addFavorite(contact:Contact) {
+    //if favorite doesn't exist:
+    const newFavorite = { ...contact};
+    this.favorites.push(newFavorite);
+
+    //if favorite exists already:
+    // this.favorites = this.favorites.filter(f => f!== contact);
   }
 
   handleClear() {
@@ -45,5 +58,7 @@ export class ContactListComponent implements OnInit {
     this.newContact.image = "";
   }
 
-  delete(contact:Contact) { this.contacts = this.contacts.filter(c => c !== contact); }
+  delete(contact:Contact) { 
+    this.contacts = this.contacts.filter(c => c !== contact); 
+  }
 }
