b13103e49d75ae7472c36dc25111b6c4f3dbf04d
Yuumi <69478059+yumyum0035@users.noreply.github.com>
2021-05-28 13:13:19 +0200

    more CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
   title = 'Contact List';
+
+  alphabetList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 }
